134fbb6df5f9f4bdf80828ed0b8f242d920079c4
RezHackXYZ <rhythmupa@gmail.com>
2025-06-28 13:13:11 -0700

    effwf;e,[wef

--- src/lib/store.js
+++ src/lib/store.js
@@ -2,3 +2,9 @@ import { writable } from "svelte/store";
 
 export let slackID = writable(null);
 export let Hackatime = writable(null);
+
+export let selectedAppName = writable(null);
+export let selectedHackatimeProjectName = writable(null);
+
+
+export let UsedHackatimeProjectName = writable(null);
--- src/lib/views/select/select.svelte
+++ src/lib/views/select/select.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { slackID } from "$lib/store";
+	import { slackID, selectedAppName, selectedHackatimeProjectName } from "$lib/store";
 	import { onMount } from "svelte";
 
 	let Apps = null;
@@ -13,23 +13,30 @@
 		Apps = res.apps;
 	});
 
-	let selectedAppName = null;
-
-	async function selectedApp(selectedAppName) {
+	async function selectedApp() {
 		const response = await fetch(
-			`https://corsproxy.io/?https://adventure-time.hackclub.dev/api/getAppUserHackatimeProjects?slackId=${$slackID}&appName=${selectedAppName.target ? selectedAppName.target.value : selectedAppName}`,
+			`https://corsproxy.io/?https://adventure-time.hackclub.dev/api/getAppUserHackatimeProjects?slackId=${$slackID}&appName=${localSelectedAppName}`,
 		);
 		const res = await response.json();
 		HackatimeProjects = res.projects;
 	}
 
-	let selectedHackatimeProjectName = null;
+	let localSelectedAppName = null;
+	let localSelectedHackatimeProjectName = null;
 </script>
 
 {#if Apps != null}
 	<div class="flex flex-col">
 		<label for="app" class="text-black dark:text-white">Select a app!</label>
-		<select class="inp" onchange={selectedApp} bind:value={selectedAppName} id="app">
+		<select
+			class="inp"
+			onchange={() => {
+				selectedAppName.set(localSelectedAppName);
+				selectedApp();
+			}}
+			bind:value={localSelectedAppName}
+			id="app"
+		>
 			<option value={null} disabled selected>Select a app!</option>
 
 			{#each Object.keys(Apps) as app}
@@ -38,12 +45,17 @@
 		</select>
 	</div>
 
-	{#if selectedAppName != null}
+	{#if $selectedAppName != null}
 		<div class="flex flex-col">
 			<label for="hackatimeProject" class="text-black dark:text-white"
-				>Select a Hackatime project for {selectedAppName}!</label
+				>Select a Hackatime project for {$selectedAppName}!</label
 			>
-			<select class="inp" bind:value={selectedHackatimeProjectName} id="hackatimeProject">
+			<select
+				class="inp"
+				onchange={() => selectedHackatimeProjectName.set(localSelectedHackatimeProjectName)}
+				bind:value={localSelectedHackatimeProjectName}
+				id="hackatimeProject"
+			>
 				<option value={null} disabled selected>Select a Hackatime project!</option>
 
 				{#each HackatimeProjects as project}
@@ -52,7 +64,7 @@
 			</select>
 		</div>
 
-		{#if selectedHackatimeProjectName != null}
+		{#if $selectedHackatimeProjectName != null}
 			<button class="btn green">Start Logging Time!</button>
 		{/if}
 	{/if}
